e4cd94ad62ad019b0152f3a10a99f044dd259595
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-10 13:59:41 +0100

    Imported abstract models and implemented contact model to get email,name,password from users

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from utils.model_abstracts import Model
+from django_extensions.db.models import (TimeStampedModel,ActivatorModel,TitleDescriptionModel)
 
-# Create your models here.
+class Contact(TimeStampedModel,ActivatorModel,TitleDescriptionModel):
+    """
+    -TimeStampModel provides us with the created field to help us know when something was created
+    -ActivatorModel helps us with the status field to know the activated and deactivated date 
+    -TitleDescriptionModel helps us with two character fields
+    """
+
+    class Meta:
+        verbose_name_plural = "Contacts"
+
+    email = models.EmailField(verbose_name = "Email")
+
+
+    def __str__(self):
+        return f'{self.title}'
+
