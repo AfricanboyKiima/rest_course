536f91bcc67851f70fae1bf58ecc9ea8cb263b08
Africanboy Kiima <fadhilkiima@gmail.com>
2024-03-01 21:58:57 +0100

    implemented serializert code for my contact model

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,3 +1,12 @@
-from . import models
 from rest_framework import serializers
 from rest_framework.fields import CharField,EmailField
+from . import models
+
+class ContactSerializer(serializers.ModelSerializer):
+    name = CharField(source="title", required = True)
+    email = EmailField( required = True)
+    message = CharField(source="description",required=True)
+    
+    class Meta:
+        model = models.Contact
+        fields = ('name','email','message')
